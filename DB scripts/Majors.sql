6a8861f0cd07419fa4036f2cda79eeedf42e546f
hiroki94 <hiroki.hayashi1@marist.edu>
2015-12-12 18:20:21 -0500

    INSERT Statements
    Added INSERT INTO scripts for Majors and Schools tables

---
+++ DB scripts/Majors.sql
@@ -0,0 +1,84 @@
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (1, 'temp', 'Accounting', 5);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (2, 'temp', 'American Studies', 4);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (3, 'temp', 'Applied Mathematics', 2);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (4, 'temp', 'Art and Art History', 1);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (5, 'temp', 'Athletic Training', 6);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (6, 'temp', 'Biology/BioMedical Sciences', 6);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (7, 'temp', 'Business Administration', 5);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (8, 'temp', 'Chemistry and Biochemistry', 6);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (9, 'temp', 'Communication', 1);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (10, 'temp', 'Computer Science', 2);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (11, 'temp', 'Criminal Justice', 7);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (12, 'temp', 'Digital Media', 1);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (13, 'temp', 'Economics', 5);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (14, 'temp', 'Education', 7);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (15, 'temp', 'English', 4);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (16, 'temp', 'Environmental Science & Policy', 6);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (17, 'temp', 'Fashion Design & Merchandising', 1);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (18, 'temp', 'History', 4);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (19, 'temp', 'Information Techonology & Systems', 2);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (20, 'temp', 'Languages', 4);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (21, 'temp', 'Liberal Studies', 3);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (22, 'temp', 'Mathematics', 2);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (23, 'temp', 'Media Studies and Production', 1);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (24, 'temp', 'Medical Technology/Medical Laboratory Sciences', 6);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (25, 'temp', 'Philosophy', 4);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (26, 'temp', 'Political Science', 4);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (27, 'temp', 'Psychology', 7);
+INSERT INTO majors(
+            majorid, concentration, majorname, schoolid)
+    VALUES (28, 'temp', 'Social Work', 7);
---
+++ DB scripts/Schools.sql
@@ -0,0 +1,21 @@
+INSERT INTO schools(
+            schoolid, schoolname, schooldean)
+    VALUES (1, 'Communication & the Arts', 'Lyn Lepre');
+INSERT INTO schools(
+            schoolid, schoolname, schooldean)
+    VALUES (2, 'Computer Science & Mathematics', 'Dr. Roger L. Norton');
+INSERT INTO schools(
+            schoolid, schoolname, schooldean)
+    VALUES (3, 'Professional Programs', 'Joel Baldomir');
+INSERT INTO schools(
+            schoolid, schoolname, schooldean)
+    VALUES (4, 'Liberal Arts', 'Dr. Martin Shaffer');
+INSERT INTO schools(
+            schoolid, schoolname, schooldean)
+    VALUES (5, 'Management', 'Lawrence G. Singleton');
+INSERT INTO schools(
+            schoolid, schoolname, schooldean)
+    VALUES (6, 'Science', 'Dr. James W. DuMond, Jr.');
+INSERT INTO schools(
+            schoolid, schoolname, schooldean)
+    VALUES (7, 'Social & Behaioral Sciences', 'Margaret R. Calista');
