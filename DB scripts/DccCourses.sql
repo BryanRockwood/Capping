a6d125e91d3a7208589759fdbd21b06078bb3ae2
hiroki94 <hiroki.hayashi1@marist.edu>
2015-12-13 12:43:57 -0500

    Marist and DccCourses Added

---
+++ DB scripts/DccCourses.sql
@@ -0,0 +1,27 @@
+INSERT INTO dcccourses(
+            dcccourseid, dcccourse_name, dcccourse_subject, dcccourse_number, dcccourse_credit)
+    VALUES (1, 'Composition II', 'ENG', 102, 3);
+INSERT INTO dcccourses(
+            dcccourseid, dcccourse_name, dcccourse_subject, dcccourse_number, dcccourse_credit)
+    VALUES (2, 'Analytic Geometry and Calculus I', 'MAT', 221, 4);
+INSERT INTO dcccourses(
+            dcccourseid, dcccourse_name, dcccourse_subject, dcccourse_number, dcccourse_credit)
+    VALUES (3, 'Analytic Geometry and Calculus II', 'MAT', 222, 4);
+INSERT INTO dcccourses(
+            dcccourseid, dcccourse_name, dcccourse_subject, dcccourse_number, dcccourse_credit)
+    VALUES (4, 'Advanced Programming Techniques', 'CPS', 142, 3);
+INSERT INTO dcccourses(
+            dcccourseid, dcccourse_name, dcccourse_subject, dcccourse_number, dcccourse_credit)
+    VALUES (5, 'Data Structures', 'CPS', 231, 3);
+INSERT INTO dcccourses(
+            dcccourseid, dcccourse_name, dcccourse_subject, dcccourse_number, dcccourse_credit)
+    VALUES (6, 'Lifetime Wellness and Fitness', 'WFE', 101, 3);
+INSERT INTO dcccourses(
+            dcccourseid, dcccourse_name, dcccourse_subject, dcccourse_number, dcccourse_credit)
+    VALUES (7, 'Assembler Language Programming', 'CIS', 227, 3);
+INSERT INTO dcccourses(
+            dcccourseid, dcccourse_name, dcccourse_subject, dcccourse_number, dcccourse_credit)
+    VALUES (8, 'Discrete Mathematics', 'MAT', 214, 3);
+INSERT INTO dcccourses(
+            dcccourseid, dcccourse_name, dcccourse_subject, dcccourse_number, dcccourse_credit)
+    VALUES (9, 'Linear Algebra', 'MAT', 215, 3);
---
+++ DB scripts/MaristCourses.sql
@@ -0,0 +1,18 @@
+INSERT INTO maristcourses(
+            mcourse_id, mcourse_subject, mcourse_number, mcourse_name, mcourse_credit, majorid)
+    VALUES (1, 'ENG', '120L', 'Writing for College', 3, 15);
+INSERT INTO maristcourses(
+            mcourse_id, mcourse_subject, mcourse_number, mcourse_name, mcourse_credit, majorid)
+    VALUES (2, 'MATH', '241L', 'Calculus I', 4, 22);
+INSERT INTO maristcourses(
+            mcourse_id, mcourse_subject, mcourse_number, mcourse_name, mcourse_credit, majorid)
+    VALUES (3, 'MATH', '242L', 'Calculus II', 4, 22);
+INSERT INTO maristcourses(
+            mcourse_id, mcourse_subject, mcourse_number, mcourse_name, mcourse_credit, majorid)
+    VALUES (4, 'CMPT', '220L', 'Software Development I', 4, 10);
+INSERT INTO maristcourses(
+            mcourse_id, mcourse_subject, mcourse_number, mcourse_name, mcourse_credit, majorid)
+    VALUES (5, 'CMPT', '308L', 'Database Management', 4, 10);
+INSERT INTO maristcourses(
+            mcourse_id, mcourse_subject, mcourse_number, mcourse_name, mcourse_credit, majorid)
+    VALUES (6, 'CMPT', '422L', 'Computer Organization & Architecture', 4, 10);
